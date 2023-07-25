596be20d63c4cd6a01d928684f2bb70274d4b06d
mariamrevishvili <140517621+mariamrevishvili@users.noreply.github.com>
2023-07-25 20:08:08 +0400

    Add files via upload

---
+++ davaleba2.py
@@ -0,0 +1,19 @@
+na me= ma rai
+au rname= rei shvili
+a ge= "13"
+ci ty=(kuta isi)
+country= georgia"
+fav ourite s ubjact=en glish"
+fav ourite sport=bas ket ball"
+fav ourite fo od=lobiasni"
+hei ght=(166)
+fav ourite ani mal= dog"
+lea st fav orite ani mal= c at'
+print h ello
+pri nt bye'
+pri nt ho w are you
+pr int ( howw)
+we ather = sunn y 
+pri nt hi i 
+bes st way to re lax= sl eep 
+fav ourite hoby= draw"
